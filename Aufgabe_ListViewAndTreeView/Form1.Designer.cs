1b30b020a35863e2edcf1dcd428f948039d54070
Vilasio <thomasstoifl@gmx.at>
2018-12-10 17:33:53 +0100

    Add project files.

---
+++ Aufgabe_ListViewAndTreeView/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace Aufgabe_ListViewAndTreeView
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeaderTrackNo = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeaderSong = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeaderDuration = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeaderAlbum = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.labelInterpret = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(10, 7);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(253, 600);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.treeView1_MouseDoubleClick);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeaderTrackNo,
+            this.columnHeaderSong,
+            this.columnHeaderDuration,
+            this.columnHeaderAlbum});
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(269, 83);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(711, 524);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeaderTrackNo
+            // 
+            this.columnHeaderTrackNo.Text = "Track No";
+            // 
+            // columnHeaderSong
+            // 
+            this.columnHeaderSong.Text = "Song";
+            this.columnHeaderSong.Width = 240;
+            // 
+            // columnHeaderDuration
+            // 
+            this.columnHeaderDuration.Text = "Duration";
+            // 
+            // columnHeaderAlbum
+            // 
+            this.columnHeaderAlbum.Text = "Album";
+            this.columnHeaderAlbum.Width = 240;
+            // 
+            // labelInterpret
+            // 
+            this.labelInterpret.AutoSize = true;
+            this.labelInterpret.Location = new System.Drawing.Point(282, 27);
+            this.labelInterpret.Name = "labelInterpret";
+            this.labelInterpret.Size = new System.Drawing.Size(53, 13);
+            this.labelInterpret.TabIndex = 2;
+            this.labelInterpret.Text = "Unknown";
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1090, 609);
+            this.Controls.Add(this.labelInterpret);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.treeView1);
+            this.Name = "FormMain";
+            this.Text = "AufgabeTreeViewAndListView";
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeaderTrackNo;
+        private System.Windows.Forms.ColumnHeader columnHeaderSong;
+        private System.Windows.Forms.ColumnHeader columnHeaderDuration;
+        private System.Windows.Forms.ColumnHeader columnHeaderAlbum;
+        private System.Windows.Forms.Label labelInterpret;
+    }
+}
+
---
+++ Aufgabe_ListViewAndTreeView/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aufgabe_ListViewAndTreeView
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            //this.listView1.View = View.Details;
+            List<List<string>> rowlist = new List<List<string>>();
+            rowlist.Add(new List<string> { "01", "Toller Song", "3:42", "Best Album" });
+            rowlist.Add(new List<string> { "02", "Tollerer Song", "6:00", "Best Album" });
+            rowlist.Add(new List<string> { "03", "Toller Song(DJ Wasweißich Remix)", "4:12", "Best Album Aniversary" });
+            //Listen befüllen
+
+
+            foreach(List<string> sublist in rowlist)//Listview aufbauen
+            {
+                ListViewItem list = new ListViewItem();
+                list.Text = sublist.ElementAt(0);
+                list.SubItems.Add(sublist.ElementAt<string>(1));
+                list.SubItems.Add(sublist.ElementAt<string>(2));
+                list.SubItems.Add(sublist.ElementAt<string>(3));
+                this.listView1.Items.Add(list);
+                
+            }
+
+            foreach(List<string> sublist in rowlist) //Treeview aufbauen
+            {
+                TreeNode node = this.treeView1.Nodes.Add(sublist.ElementAt<string>(3));
+                
+                node.Nodes.Add(sublist.ElementAt<string>(1));
+            }
+        }
+
+        private void treeView1_MouseDoubleClick(object sender, MouseEventArgs e) //Das betreffende Element in Listview auswählen je nach dem was in Treevie mit Doppelklick ausgewählt wird
+        {
+            for (int i = 0; i < this.listView1.Items.Count; i++)
+            {
+                listView1.Items[i].Selected = false;
+            }
+            TreeNode node =  this.treeView1.GetNodeAt(e.X, e.Y);
+            ListViewItem item = (this.listView1.FindItemWithText(node.Text));
+            //item.BackColor = Color.LightSteelBlue;
+            listView1.Items[item.Index].Selected = true;
+            listView1.Select();
+
+        }
+    }
+}
